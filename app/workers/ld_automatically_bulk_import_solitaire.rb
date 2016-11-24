4059b7f49c19fb7d79c6dba271319b1eedc0eb72
Chetan Gupta <chetangupta89@gmail.com>
2016-11-24 22:13:51 +0530

    UPdated API tokens

--- app/workers/ld_automatically_bulk_import_solitaire.rb
+++ app/workers/ld_automatically_bulk_import_solitaire.rb
@@ -3,17 +3,18 @@ class LDAutomaticallyBulkImportSolitaire
   def self.perform()
     Rails.logger.warn  ">>>>>>>>  LDAutomaticallyBulkImportSolitaire processing started"
     suppliers = [
-      {name: "bluestar", access_token: "579af7e10132f46daffaf8b90a05eb23"},
-      {name: "venusftp", access_token: "7da7009a595af36b2641414918c024e7"},
-      {name: "ankitgems", access_token: "162e66209b8ac2c7f74bcba0291b1f7b"},
-      {name: "kiran", access_token: "42572a1da99f7cb49c7fbcc1eb2c18cd"},
-      {name: "harikrishna", access_token: "ddf7d52543be7ea8f8c303661bec31b1"},
-      {name: "kgirdharlal", access_token: "8fa11267cd1248968815b120f7f00e23"},
-      {name: "jewelex", access_token: "873097a79519ebde8a3620416dbdaf6f"},
-      {name: "rosyblue", access_token: "06250b814d4925de443faf29f77bbcc9"},
-      {name: "shreeramkrishna", access_token: "4a0abea2bc1fb8fb420bfc49de28be56"},
+      {name: "bluestar", access_token: "8ccf8b94cba2986088a2cc5147487ec4"},
+      {name: "venusftp", access_token: "2c7d667820f3c8b7a3369fc0bde67195"},
+      {name: "ankitgems", access_token: "d8cecd66998578d89ae1c614ad83d821"},
+      {name: "kiran", access_token: "a63210ead6d96dc15efda1ef252c9457"},
+      {name: "harikrishna", access_token: "f46700ffb203bcd322e3cb9003ee2efb"},
+      {name: "kgirdharlal", access_token: "a38f122abda8861bf9bdb28342824601"},
+      {name: "jewelex", access_token: "3030aad2c090dd87b7791afc135ac3a3"},
+      {name: "rosyblue", access_token: "a0e57fece79ee65c4f12c4c503753bd6"},
+      {name: "shreeramkrishna", access_token: "9eb7a75603f3556d20709a5608842450"},
+      # {name: "kc", access_token: "95bd43d9ebe6bfd247a491a16d07d4ea"},
+      # {name: "shairu", access_token: "6e080274c2213873def71877ce537a97"},
     ]
-
     suppliers.each do |supplier|
       begin
         directory = Rails.root.join('public', "ftp_upload/#{supplier[:name]}")
